9a0fcf362f299a4215462960095bc8b583e39d75
SamuelBernard4 <142609309+SamuelBernard4@users.noreply.github.com>
2023-08-25 12:25:49 -0400

    Update metadata_treat.py

--- metadata_treat.py
+++ metadata_treat.py
@@ -16,9 +16,9 @@ df['reads'] = ((df['Bases'] / (df['AvgSpotLen'] * 2))).apply(np.floor).astype(in
 
 # Separate data into cancer and control groups and save to separate files
 df_cancer = df[df['disease'] == 'COLORECTAL CANCER']
-df_cancer.to_csv(r'c:\users\jeremie\desktop\df_cancer_IC.txt', index=None)
+df_cancer.to_csv(r'c:\users\jeremie\desktop\df_cancer.txt', index=None)
 print(df_cancer.head())
 
 df_control = df[df['disease'] == 'CONTROL']
-df_control.to_csv(r'c:\users\jeremie\desktop\df_control_IC.txt', index=None)
+df_control.to_csv(r'c:\users\jeremie\desktop\df_control.txt', index=None)
 print(df_control.head())
